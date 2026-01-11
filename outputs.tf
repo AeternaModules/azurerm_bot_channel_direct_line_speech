37e9b3836dd37de8f0bcdcdeee20c71e6baf70ba
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:32:46 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "bot_channel_direct_line_speechs" {
+  description = "All bot_channel_direct_line_speech resources"
+  value       = azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speechs
+}
+
