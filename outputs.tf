f7ebd82c8f77ce65ea980af6a7d868f81f404b81
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,34 @@
-output "bot_channel_direct_line_speeches" {
-  description = "All bot_channel_direct_line_speech resources"
-  value       = azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches
-  sensitive   = true
-}
 output "bot_channel_direct_line_speeches_bot_name" {
-  description = "List of bot_name values across all bot_channel_direct_line_speeches"
-  value       = [for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : v.bot_name]
+  description = "Map of bot_name values across all bot_channel_direct_line_speeches, keyed the same as var.bot_channel_direct_line_speeches"
+  value       = { for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : k => v.bot_name }
 }
 output "bot_channel_direct_line_speeches_cognitive_account_id" {
-  description = "List of cognitive_account_id values across all bot_channel_direct_line_speeches"
-  value       = [for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : v.cognitive_account_id]
+  description = "Map of cognitive_account_id values across all bot_channel_direct_line_speeches, keyed the same as var.bot_channel_direct_line_speeches"
+  value       = { for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : k => v.cognitive_account_id }
 }
 output "bot_channel_direct_line_speeches_cognitive_service_access_key" {
-  description = "List of cognitive_service_access_key values across all bot_channel_direct_line_speeches"
-  value       = [for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : v.cognitive_service_access_key]
+  description = "Map of cognitive_service_access_key values across all bot_channel_direct_line_speeches, keyed the same as var.bot_channel_direct_line_speeches"
+  value       = { for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : k => v.cognitive_service_access_key }
   sensitive   = true
 }
 output "bot_channel_direct_line_speeches_cognitive_service_location" {
-  description = "List of cognitive_service_location values across all bot_channel_direct_line_speeches"
-  value       = [for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : v.cognitive_service_location]
+  description = "Map of cognitive_service_location values across all bot_channel_direct_line_speeches, keyed the same as var.bot_channel_direct_line_speeches"
+  value       = { for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : k => v.cognitive_service_location }
 }
 output "bot_channel_direct_line_speeches_custom_speech_model_id" {
-  description = "List of custom_speech_model_id values across all bot_channel_direct_line_speeches"
-  value       = [for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : v.custom_speech_model_id]
+  description = "Map of custom_speech_model_id values across all bot_channel_direct_line_speeches, keyed the same as var.bot_channel_direct_line_speeches"
+  value       = { for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : k => v.custom_speech_model_id }
 }
 output "bot_channel_direct_line_speeches_custom_voice_deployment_id" {
-  description = "List of custom_voice_deployment_id values across all bot_channel_direct_line_speeches"
-  value       = [for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : v.custom_voice_deployment_id]
+  description = "Map of custom_voice_deployment_id values across all bot_channel_direct_line_speeches, keyed the same as var.bot_channel_direct_line_speeches"
+  value       = { for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : k => v.custom_voice_deployment_id }
 }
 output "bot_channel_direct_line_speeches_location" {
-  description = "List of location values across all bot_channel_direct_line_speeches"
-  value       = [for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : v.location]
+  description = "Map of location values across all bot_channel_direct_line_speeches, keyed the same as var.bot_channel_direct_line_speeches"
+  value       = { for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : k => v.location }
 }
 output "bot_channel_direct_line_speeches_resource_group_name" {
-  description = "List of resource_group_name values across all bot_channel_direct_line_speeches"
-  value       = [for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : v.resource_group_name]
+  description = "Map of resource_group_name values across all bot_channel_direct_line_speeches, keyed the same as var.bot_channel_direct_line_speeches"
+  value       = { for k, v in azurerm_bot_channel_direct_line_speech.bot_channel_direct_line_speeches : k => v.resource_group_name }
 }
 
